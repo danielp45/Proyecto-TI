1718bf1ec87c3bac920a6b49c3eb846d6d367bf1
Daniel Polanco <carlos.polancodz@gmail.com>
2025-10-29 14:38:03 -0500

    tbprofilerchain.sh

--- tbprofilerchain.sh
+++ tbprofilerchain.sh
@@ -1,29 +1,26 @@
+#Opcion 1
 FASTA_DIR="draft_assemblies"      
-OUT_DIR="resultados_TBP"          
-mkdir -p "$OUT_DIR"               
-
+OUT_DIR="resultados_TBP"
+mkdir -p "$OUT_DIR"  
 for fasta in "$FASTA_DIR"/*.fasta; do
     id=$(basename "$fasta".fasta)
     echo "Procesando ${id}..."
     tb-profiler profile --fasta "$fasta".fasta -p "$OUT_DIR/tb-${id}"
 done
-
 echo "✅ Análisis completado. Resultados en: $OUT_DIR/"
 
-
+#Opcion 2(mejor desarrollada)
 #!/bin/bash
 FASTA_DIR="draft_assemblies"
 OUT_DIR="resultados_TBP"
-
 # Crear carpeta de salida si no existe
 mkdir -p "$OUT_DIR"
-
 # Recorrer todos los archivos con extensión .fasta
 for fasta in "$FASTA_DIR"/*.fasta; do
-    id=$(basename "$fasta" .fasta)   # quitar extensión .fasta
+    id=$(basename "$fasta".fasta)   # quitar extensión .fasta
     echo "🔹 Procesando ${id}..."
     tb-profiler profile --fasta "$fasta" -p "$OUT_DIR/tb-${id}"
 done
-
 echo "✅ Análisis completado. Resultados en: $OUT_DIR/"
+
 
